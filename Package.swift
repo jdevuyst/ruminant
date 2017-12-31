1e47dafe0cb37df964e340bac35223ea0a0e2b24
Stefan Boether <stefan.boether@gmail.com>
2017-12-31 15:28:45 +0100

    makes the package manifest Swift Tools 4.0 compatible

--- Package.swift
+++ Package.swift
@@ -3,5 +3,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "ruminant"
+    name: "ruminant",
+    
+    products: [
+        .library(
+            name: "ruminant",
+            targets: ["ruminant"]),
+    ],
+    
+    dependencies: [
+    ],
+    
+    targets: [
+        .target(
+            name: "ruminant",
+            path: ".",
+            sources: ["Sources"]),
+        
+        .testTarget(
+            name: "ruminantTests",
+            dependencies: ["ruminant"],
+            path: "./Tests",
+            sources: ["ruminantTests"]),
+        ]
 )
